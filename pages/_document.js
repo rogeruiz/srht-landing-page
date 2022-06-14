6d7dd2b48c752ae221dce1d10cf3b31b8a27f68a
Roger Steve Ruiz <hi@rog.gr>
2022-06-14 16:25:33 -0400

    Add the preconnect links too

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,12 @@ export default class Document extends NextDocument {
     return (
       <Html lang="en">
         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Fjalla+One&family=Joan&family=VT323&display=swap"
             rel="stylesheet"
