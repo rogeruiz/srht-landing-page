299a8da9ce9c6449bfabb6ca0bbab33206a7f6ca
Roger Steve Ruiz <hi@rog.gr>
2022-06-14 17:02:30 -0400

    Update things and try a build

---
+++ next.config.js
@@ -0,0 +1,24 @@
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline';
+  style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+  font-src 'self' https://fonts.gstatic.com;
+`
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+  }
+]
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/(.*?)',
+        headers: securityHeaders
+      }
+    ]
+  }
+}
