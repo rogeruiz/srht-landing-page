2601251e1ae9bef70f43762066aa4a2dc1a6f317
Roger Steve Ruiz <hi@rog.gr>
2022-06-14 00:59:32 -0400

    Modify securityHeaders

---
+++ next.config.js
@@ -0,0 +1,25 @@
+const ContentSecurityPolicy = `
+  default-src 'self' 'unsafe-inline';
+  script-src 'self' 'unsafe-inline' 'unsafe-eval';
+  style-src 'self' fonts.gstatic.com 'unsafe-inline';
+  font-src 'self' fonts.gstatic.com 'unsafe-inline';
+`
+
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+  }
+]
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders
+      }
+    ]
+  }
+}
