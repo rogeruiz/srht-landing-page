36c9edba865890cb22d59e7e437752b25f58f945
Roger Steve Ruiz <hi@rog.gr>
2022-06-14 00:06:12 -0400

    Redesign the homepage a bit

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,31 @@
 import { Box, Container, Heading, Stack, Text, Link } from '@chakra-ui/react'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
 import ThemeToggleButton from '../components/theme-toggle-button'
 
 const Home = () => {
   return (
     <Container>
-      <Container
-        display="flex"
-        p={2}
-        maxW="container.lg"
-        wrap="wrap"
-        align="center"
-        justify="space-between"
-      >
-        <Box flex={2} align="left">
-          <Heading as="h1" size="md" textTransform={'uppercase'}>
-            Roger Steve Ruiz has a landing page
+      <ThemeToggleButton />
+      <Stack spacing={4} mt={'1rem'}>
+        <Box>
+          <Heading
+            borderBottom={'1px'}
+            as="h1"
+            size="lg"
+            textTransform={'uppercase'}
+          >
+            Landing page
           </Heading>
         </Box>
-        <Box flex={1} align="right">
-          <ThemeToggleButton />
+        <Box>
+          <Text fontSize="3xl">
+            I&lsquo;m Roger Steve Ruiz, an L4 Senior Engineer at
+            <Link textDecor={'underline'} href="https://truss.works/">
+              TrussWorks <ExternalLinkIcon fontSize={'1rem'} mr="6px" />
+            </Link>
+            building user-centered digital services.
+          </Text>
         </Box>
-      </Container>
-      <Stack spacing={3} mt={'1rem'}>
-        <Text fontSize="3xl">
-          I&lsquo;m Roger Steve Ruiz, an L4 Senior Engineer at
-          <Link href="https://truss.works/">TrussWorks</Link>
-          building user-centered digital services.
-        </Text>
       </Stack>
     </Container>
   )
