99fe1bab5a2349d6943a6dfcaca4e47f3c45503e
Roger Steve Ruiz <hi@rog.gr>
2022-06-15 00:47:02 -0400

    Talk about the tools used to build this

--- components/footer.js
+++ components/footer.js
@@ -1,9 +1,10 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Text } from '@chakra-ui/react'
 
 const Footer = () => {
   return (
     <Box align="center" mt={'2rem'} opacity={0.4} fontSize="sm">
-      {new Date().getFullYear()} Roger Steve Ruiz
+      <Text>{new Date().getFullYear()} Roger Steve Ruiz</Text>
+      <Text>Built with NextJS, Chakra-UI, and Framer Motion</Text>
     </Box>
   )
 }
