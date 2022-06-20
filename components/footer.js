e2b9572a045e3edd57aa7379306b9a860b57bac3
Roger Steve Ruiz <hi@rog.gr>
2022-06-20 01:39:10 -0400

    Add a link back to sourcehut in the footer

--- components/footer.js
+++ components/footer.js
@@ -1,10 +1,12 @@
-import { Box, Text } from '@chakra-ui/react'
+import { Link, Box, Text } from '@chakra-ui/react'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
 
 const Footer = () => {
   return (
     <Box align="center" mt={'2rem'} opacity={0.4} fontSize="sm">
       <Text>{new Date().getFullYear()} Roger Steve Ruiz</Text>
       <Text>Built with NextJS, Chakra-UI, and Framer Motion</Text>
+      <Text>Deployed on <Link href="https://srht.site/">sourcehut Pages <ExternalLinkIcon fontSize={'1rem'} mr="3px" /></Link></Text>
     </Box>
   )
 }
