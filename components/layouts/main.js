761ba58a798f0e1d793771296a2d1033c588721f
Roger Steve Ruiz <hi@rog.gr>
2023-09-27 13:45:29 -0500

    Despedir la vaina;
    La bandera que demuestra que el website esta bajo de construcción la elimine.

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,13 +1,12 @@
 import { Box, Container } from '@chakra-ui/react'
 import Head from 'next/head'
 import Footer from '../footer'
-import UnderConstructionBanner from '../under-construction-banner.js'
+// import UnderConstructionBanner from '../under-construction-banner.js'
 
 const Main = ({ children }) => {
   return (
     <Box as="main" pb={8}>
       <Head></Head>
-      <UnderConstructionBanner />
       <Container maxW="container.xl" pt={12}>
         {children}
         <Footer />
