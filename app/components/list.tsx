5ae0d8e2fc72de561831f24da44198f6f83ecc69
Roger Steve Ruiz <hi@rog.gr>
2023-12-09 02:05:21 -0600

    Progresando más en camino pa' desplegar el website

---
+++ app/components/list.tsx
@@ -0,0 +1,50 @@
+// NextJS SSG Landing Page
+// Copyright (C) 2023  Roger Steve Ruiz
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+interface Link {
+  name: string
+  href: string
+  desc: string
+}
+
+interface Links extends Array<Link> {}
+
+export default function List({ title, data }: { title: string; data: Links }) {
+  const listItems = data.map((li: Link, idx: number) => {
+    return (
+      <li key={idx} className="list-none hover:list-disc my-1">
+        <a
+          className="underline text-yellow hover:text-teal visited:text-lavender block"
+          href={li.href}
+          title={li.desc}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {li.name}
+        </a>
+      </li>
+    )
+  })
+
+  return (
+    <div className="bg-mantle p-8 rounded mb-10 md:mb-0">
+      <h4 className="text-overlay2 shadow-surface2 text-xl font-display uppercase mb-5">
+        {title}
+      </h4>
+      <ul className="mb-12">{listItems}</ul>
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,36 @@
-// import Image from 'next/image'
+// NextJS SSG Landing Page
+// Copyright (C) 2023  Roger Steve Ruiz
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-export default function Home() {
+import List from './components/list'
+import { getDaily, getSkills } from './lib/fetch-links'
+
+export default async function Home() {
+  const linkData = await getDaily()
+  const skillData = await getSkills()
+
   return (
-    <main className="bg-base text-text flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="font-display text-9xl">Hola, mundo!</h1>
-      <p className="font-sans text-lg">
-        Esto es unas palabras para que tomen espacio.
-      </p>
-      <code>console.log('hello, world.')</code>
+    <main className="bg-base text-text min-h-screen p-8 md:p-24">
+      <section className="md:grid grid-cols-4 gap-4">
+        <List title="daily links" data={linkData.daily} />
+        <List title="programming languages" data={skillData.languages} />
+        <List title="frameworks" data={skillData.frameworks} />
+        <List title="package managers" data={skillData.package_managers} />
+        <List title="configuration languages" data={skillData.configuration} />
+      </section>
+      <p className="mt-16">Roger Steve Ruiz is a software engineer</p>
     </main>
   )
 }
